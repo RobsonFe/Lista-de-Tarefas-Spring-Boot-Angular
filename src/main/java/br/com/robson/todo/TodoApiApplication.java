c42cdd23c80652a9dc2b39a2edc843c5fd9330f3
RobsonFe <robson_20ferreira@hotmail.com>
2023-10-04 13:12:52 -0300

    configurando o banco de dados

--- src/main/java/br/com/robson/todo/TodoApiApplication.java
+++ src/main/java/br/com/robson/todo/TodoApiApplication.java
@@ -1,10 +1,12 @@
 package br.com.robson.todo;
 
 import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import br.com.robson.todo.model.Todo;
 import br.com.robson.todo.repository.TodoRepository;
@@ -15,26 +17,21 @@ public class TodoApiApplication {
 	@Autowired
 	private TodoRepository todoRepository;
 	
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApiApplication.class, args);
+	}
+	
+	@Bean
 	public CommandLineRunner init() {
 		return new CommandLineRunner() {
-			
 			@Override
 			public void run(String... args) throws Exception {
 				Todo todo = new Todo();
 				todo.setDescription("Estudando Spring Boot");
 				todo.setCreateDate(LocalDateTime.now());
 				todoRepository.save(todo);
-				
 			}
 		};
-		
-		
 	}
-	
-	public static void main(String[] args) {
-		SpringApplication.run(TodoApiApplication.class, args);
-		
-	}
-	
-	
 }
+
